aabbeeca29fce67b77958834b8ebd9e0a55df3f9
azuspan607 <aa.zuspan@gmail.com>
2021-02-15 14:10:44 -0800

    Implement Sankey plotting

--- geevis/lulc.py
+++ geevis/lulc.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
+import plotly.graph_objects as go
 
 from geevis import utils
 
@@ -85,9 +86,9 @@ def check_plot_params(data, labels, palette):
             f"The following values are present in the data and undefined in the palette: {missing_palette}")
 
 
-def drop_small_classes(data, max_classes):
+def drop_classes(data, max_classes, metric="area"):
     """
-    Remove small classes until a maximum number of classes is reached. Class counts are cumulative regardless of groups.
+    Remove small classes until a maximum number of classes is reached.
 
     :param pd.DataFrame data: A dataframe in which each row represents as single sample point and columns represent
     the class of that point at various times.
@@ -96,10 +97,12 @@ def drop_small_classes(data, max_classes):
     :return pd.DataFrame: A dataframe with rows that belong to the largest classes.
     """
     class_counts = data.melt().groupby("value").size().reset_index(name="n")
-    biggest_classes = class_counts.sort_values(
+    largest_classes = class_counts.sort_values(
         by="n", ascending=False).value[0:max_classes].tolist()
 
-    return data[data.isin(biggest_classes)].dropna()
+    dropped_data = data[data.isin(largest_classes)].dropna()
+
+    return dropped_data
 
 
 def plot_area(data, start_label, end_label, dataset=None, class_labels=None, class_palette=None, max_classes=5, exclude=None, normalize=True):
@@ -134,14 +137,14 @@ def plot_area(data, start_label, end_label, dataset=None, class_labels=None, cla
         class_labels = dataset.labels
         class_palette = dataset.palette
 
-    data = drop_small_classes(data, max_classes)
+    if exclude:
+        data = data[~data.isin(exclude)].dropna()
+
+    data = drop_classes(data, max_classes)
 
     # Count the frequency of each class at the start and end
     freq = data.apply(pd.Series.value_counts).reset_index().melt(
         id_vars="index", var_name="label", value_name="n").fillna(0)
-
-    if exclude:
-        freq = freq[~freq["index"].isin(exclude)]
 
     # Check for missing values in labels or palette
     check_plot_params(freq, class_labels, class_palette)
@@ -179,4 +182,79 @@ def plot_area(data, start_label, end_label, dataset=None, class_labels=None, cla
                fontsize=16,
                frameon=False
                )
+    return fig
+
+
+def plot_sankey(data, start_label, end_label, dataset=None, class_labels=None, class_palette=None, max_classes=5, exclude=None, normalize=True):
+    """
+    Generate a stacked area plot showing how the sampled area of cover changed from a start condition to an end
+    condition.
+
+    :param pd.DataFrame data: A dataframe in which each row represents as single sample point with the starting class
+    in the "start" column and the ending class in the "end" column.
+    :param str start_label: A label to describe the starting conditions, such as "prefire" or "2012".
+    :param str end_label: A label to describe the ending conditions, sucsh as "postfire" or "2015".
+    :param geevis.dataset.Dataset dataset: A dataset from which the class data was generated, containing labels and
+    palettes corresponding to class values. If a dataset is not provided, class labels and a class palette must be
+    provided instead.
+    :param dict class_labels: A dictionary where keys are the class index values and the values are corresponding
+    labels. Every class index in the sample dataset must be included in class_labels.
+    :param dict class_palette: A dictionary where keys are the class index values and the values are corresponding
+    colors. Every class index in the sample dataset must be included in class_palette.
+    :param int max_classes: The maximum number of unique classes to include in the plot. If more classes are present,
+    the unimportant classes will be omitted from the plot.
+    :param list exclude: A list of class values to remove from the plot.
+    :param bool normalize: If true, the total area in each group will be normalized to 1. If classes are removed due
+    to fit max classes, this will rescale the remaining classes.
+    """
+    if dataset is None and any([x is None for x in [class_labels, class_palette]]):
+        raise ValueError(
+            "Provide either a dataset or class labels and a class palette.")
+    elif dataset is not None and any([x is not None for x in [class_labels, class_palette]]):
+        raise ValueError(
+            "Provide only a dataset or class labels and a class palette, not both.")
+    elif dataset:
+        class_labels = dataset.labels
+        class_palette = dataset.palette
+
+    if exclude:
+        data = data[~data.isin(exclude)].dropna()
+
+    data = drop_classes(data, max_classes)
+
+    # Generate a list of unique labels
+    unique_labels = list(np.unique(data.start.to_list() + data.end.to_list()))
+
+    # Transform the data to get counts of each combination of start and end condition
+    sankey_data = data.groupby(["start", "end"]).size().reset_index(name="n")
+
+    # Create an index to refer to the start class
+    sankey_data["start_index"] = sankey_data.start.apply(
+        lambda x: unique_labels.index(x))
+    # Create an index to refer to the end class. The end class must have a different value than the same start class,
+    # so it is offset by the number of unique class labels
+    sankey_data["end_index"] = sankey_data.end.apply(
+        lambda x: unique_labels.index(x)) + len(unique_labels)
+
+    # You need to have one label for each class (start and end)
+    sankey_labels = [class_labels[i] for i in unique_labels] * 2
+    # You only need one color for each source class
+    sankey_colors = [class_palette[i] for i in sankey_data.start]
+
+    fig = go.Figure(data=[go.Sankey(
+        arrangement="snap",
+        node=dict(
+            line=dict(color="black", width=0.5),
+            label=sankey_labels,
+            color=[class_palette[i] for i in unique_labels] * 2
+        ),
+        link=dict(
+            # These values refer to the index of the labels
+            source=sankey_data.start_index,
+            target=sankey_data.end_index,
+            value=sankey_data.n,
+            color=sankey_colors,
+            hovertemplate='%{value}% changed from %{source.label} to %{target.label}'
+        ))])
+
     return fig
