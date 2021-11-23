5e280cfe14bc09479d31c2babc8ec65fd66fd1c5
Aaron Zuspan <aa.zuspan@gmail.com>
2021-11-22 21:58:37 -0800

    Bump version: 0.0.5 → 0.0.6

--- docs/conf.py
+++ docs/conf.py
@@ -20,7 +20,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'sankee'
 copyright = '2021, Aaron Zuspan'
 author = 'Aaron Zuspan'
-version = 'v0.0.5'
+version = 'v0.0.6'
 
 
 # -- General configuration ---------------------------------------------------
