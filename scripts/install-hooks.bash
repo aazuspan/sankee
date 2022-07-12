36f4347af50ded4e71434a141c3f80c6ee935e7a
Aaron Zuspan <aa.zuspan@gmail.com>
2022-07-12 15:33:13 -0700

    Remove scripts (replaced by Makefile)

--- scripts/install-hooks.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Run this script once to tell Git where to find hooks.
-
-echo "Setting git hooks directory..."
-# Set the hooks directory
-git config core.hooksPath .git-hooks
-
-echo "Git hooks directory set successfully!"
