2971c50f3c218ab3e5f851733f6596211ad79970
Aaron Zuspan <aa.zuspan@gmail.com>
2025-06-14 07:56:44 -0700

    Authenticate with service account in CI

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,23 @@
+import json
+import os
+
 import ee
 
-ee.Initialize()
+
+def pytest_sessionstart(session):
+    _init_ee_for_tests()
+
+
+def _init_ee_for_tests():
+    # Use the Github Service Account for CI tests
+    if os.environ.get("GITHUB_ACTIONS"):
+        key_data = os.environ.get("EE_SERVICE_ACCOUNT")
+        project_id = json.loads(key_data).get("project_id")
+        credentials = ee.ServiceAccountCredentials(None, key_data=key_data)
+    # Use stored persistent credentials for local tests
+    else:
+        # Project should be parsed from credentials
+        project_id = None
+        credentials = "persistent"
+
+    ee.Initialize(credentials, project=project_id)
